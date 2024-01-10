bb7bd6e7366b31fd4568c25afca77b3dac4443e3
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2024-01-11 00:42:11 +0200

    loginPage

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,61 @@
+<template>
+<q-page class="flex flex-center">
+  <div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="password"
+        label="Password *"
+      />
+
+      <!-- <q-toggle v-model="accept" label="I accept the license and terms" /> -->
+
+      <div>
+        <q-btn label="Login" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+
+  </div>
+</q-page>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoginPage',
+  data () {
+    return {
+      name: '',
+      password: ''
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      console.log('@submit - do something here', evt)
+      evt.target.submit()
+    },
+    onReset () {
+      console.log('@reset - ^_^')
+      // eslint-disable-next-line no-global-assign
+      this.name = null
+      // eslint-disable-next-line no-undef
+      this.age = null
+      // eslint-disable-next-line no-undef
+      this.accept = false
+    }
+  }
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,13 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
   },
+  {
+    path: '/Login',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/LoginPage.vue') }
+    ]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
